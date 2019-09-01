825dc8958b2b57e471d039271fae0dcb28cd8ae4
Robel <robelseyoum3@gmail.com>
2019-09-01 13:13:27 +0100

    adding fragemnt and make retrofit call from fragment

--- app/src/main/java/com/robelseyoum3/weekendexcercise/views/mainview/MainActivity.kt
+++ app/src/main/java/com/robelseyoum3/weekendexcercise/views/mainview/MainActivity.kt
@@ -8,6 +8,7 @@ import com.robelseyoum3.weekendexcercise.common.Constants
 import com.robelseyoum3.weekendexcercise.models.NewsSource
 import com.robelseyoum3.weekendexcercise.network.NewsRequest
 import com.robelseyoum3.weekendexcercise.network.RetrofitInstances
+import com.robelseyoum3.weekendexcercise.views.mainview.fragmentviews.MainFragment
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,29 +19,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val newsRequest = RetrofitInstances().retrofitInstances.create(NewsRequest::class.java)
-
-        val call = newsRequest.getNews(Constants.COUNTRY, Constants.API_KEY)
-
-        call.enqueue(object : Callback<NewsSource>{
-
-            override fun onFailure(call: Call<NewsSource>, t: Throwable) {
-                Log.d("News Error ", ""+t.message)
-
-            }
-
-            override fun onResponse(call: Call<NewsSource>, response: Response<NewsSource>) {
-                val res = response.body()
-                Log.d("Title ", res!!.articles[0].author)
-            }
-
-//https://newsapi.org/v2/top-headlines?country=us&apiKey=9965f6d322474e2c93a08da812d621d6
-//https://newsapi.org/v2/top-headlines?country=us&api_key=9965f6d322474e2c93a08da812d621d6
-//https://newsapi.org/v2/top-headlines?country=us&apiKey=9965f6d322474e2c93a08da812d621d6
-//https://newsapi.org/v2/top-headlines?country=us&apikey=9965f6d322474e2c93a08da812d621d6
-
-        })
-
-
+        addFragment()
     }
+
+    private fun addFragment(){
+        var fragmentManager = supportFragmentManager
+        var fragmetTransaction = fragmentManager.beginTransaction()
+
+        fragmetTransaction.add(R.id.fragment_container_from_main, MainFragment())
+            //  .addToBackStack(null)
+            .commit()
+    }
+
+
+
 }
---
+++ app/src/main/java/com/robelseyoum3/weekendexcercise/views/mainview/fragmentviews/MainFragment.kt
@@ -0,0 +1,64 @@
+package com.robelseyoum3.weekendexcercise.views.mainview.fragmentviews
+
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.robelseyoum3.weekendexcercise.R
+import com.robelseyoum3.weekendexcercise.common.Constants
+import com.robelseyoum3.weekendexcercise.models.NewsSource
+import com.robelseyoum3.weekendexcercise.network.NewsRequest
+import com.robelseyoum3.weekendexcercise.network.RetrofitInstances
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        retofitCall()
+    }
+
+    private fun retofitCall(){
+
+        val newsRequest = RetrofitInstances().retrofitInstances.create(NewsRequest::class.java)
+        val call = newsRequest.getNews(Constants.COUNTRY, Constants.API_KEY)
+        call.enqueue(object : Callback<NewsSource> {
+
+            override fun onFailure(call: Call<NewsSource>, t: Throwable) {
+                Log.d("News Error ", ""+t.message)
+            }
+
+            override fun onResponse(call: Call<NewsSource>, response: Response<NewsSource>) {
+                val res = response.body()
+                Log.i("Title ", ""+res!!.articles[1].title)
+            }
+
+        })
+    }
+
+
+}
